46bf61e6ce495754c54af6fe73cf8d50e190b63d
@wuuuw <aagilinskaya@edu.hse.ru>
2021-04-10 23:53:27 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/yadelaugit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yadelaugit.dir/main.cpp.obj"
+  "libyadelaugit.dll.a"
+  "yadelaugit.exe"
+  "yadelaugit.exe.manifest"
+  "yadelaugit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yadelaugit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World, Hi hi!" << std::endl;
+    return 0;
+}
